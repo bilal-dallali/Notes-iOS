311839c6561724626f4cb3f02a59f9e870d570f0
bilal-dallali <dallali-bilal@hotmail.fr>
2023-09-08 16:05:57 +0200

    add and load notes

--- Notes/HomeView.swift
+++ Notes/HomeView.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var notes = [Note]()
+    
+    @State var showAdd = false
+    
     var body: some View {
         NavigationView {
-            List(0..<9) { i in
-                Text("Numéro \(i)")
+            List(self.notes) { note in
+                Text(note.note)
+                    .padding()
             }
+            .sheet(isPresented: $showAdd, content: {
+                AddNoteView()
+            })
+            .onAppear(perform: {
+                fetchNotes()
+            })
             .navigationTitle("Notes")
             .navigationBarItems(trailing: Button(action: {
-                print("Add a note")
+                self.showAdd.toggle()
             }, label: {
                 Text("Add")
             }))
         }
     }
+    
+    func fetchNotes() {
+        let url = URL(string: "http://localhost:3000/notes")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, res, err in
+            guard let data = data else { return }
+            
+            do {
+                let notes = try JSONDecoder().decode([Note].self, from: data)
+                print(notes)
+                self.notes = notes
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct Note: Identifiable, Codable {
+    var id: String { _id }
+    var _id: String
+    var note: String
 }
 
 struct ContentView_Previews: PreviewProvider {
